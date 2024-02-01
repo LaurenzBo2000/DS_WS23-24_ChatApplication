18a59b45fbbc6d62d8df3744c838e79a40397b69
Schiruza <150375084+Schiruza@users.noreply.github.com>
2024-02-01 22:36:22 +0100

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -1,67 +1,55 @@
-'''
-send_heartbeat(client_socket, interval)
-
-Purpose: Sends regular heartbeat messages to the server.
-Functionality: Runs in a loop, sending a "HEARTBEAT" message to the server at regular intervals.
-Exception Handling: Handles exceptions related to lost server connection and socket issues.
-
-HeartbeatChecker Class:
-
-Key Methods:
-
-update_heartbeat(client_socket): Updates the timestamp for a client's last heartbeat.
-remove_client(client_socket): Removes a client from the heartbeat tracking, usually when a timeout is detected.
-check_heartbeats(): Regularly checks for clients that haven't sent a heartbeat within the timeout period and removes them
-'''
-
 import threading
 import time
 import logging
+import socket
+import config
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-def send_heartbeat(client_socket, stop_event, interval=5):
-    logging.info("Heartbeat thread started")
-
-    while not stop_event.is_set():
-        try:
-            client_socket.sendall(b'HEARTBEAT')
-            time.sleep(interval)
-        except (ConnectionResetError, BrokenPipeError, OSError) as e:
-            logging.error(f"Disconnected from the server. Stopping heartbeat. Error: {e}")
-            break
+logging.basicConfig(level=logging.getLevelName(config.LOG_LEVEL), format=config.LOG_FORMAT)
 
 class HeartbeatChecker:
-    def __init__(self, timeout=10):
-        self.heartbeats = {}
-        self.timeout = timeout
+    def __init__(self):
+        self.server_heartbeats = {}
         self.lock = threading.Lock()
 
-    def update_heartbeat(self, client_socket):
+    def update_server_heartbeat(self, server_socket):
         with self.lock:
-            self.heartbeats[client_socket] = time.time()
-            logging.info(f"Heartbeat updated for {client_socket.getpeername()}")
-
-    def remove_client(self, client_socket):
-        with self.lock:
-            if client_socket in self.heartbeats:
-                del self.heartbeats[client_socket]
-                logging.info(f"Client {client_socket.getpeername()} removed from heartbeat tracking")
+            self.server_heartbeats[server_socket] = time.time()
+            logging.info(f"Server heartbeat updated for {server_socket.getpeername()}")
 
     def check_heartbeats(self):
         current_time = time.time()
         with self.lock:
-            to_remove = [client_socket for client_socket, timestamp in self.heartbeats.items()
-                         if current_time - timestamp > self.timeout]
-            for client_socket in to_remove:
-                logging.warning(f"Client {client_socket.getpeername()} timed out.")
-                self.remove_client(client_socket)
-                try:
-                    client_socket.close()
-                except OSError as e:
-                    logging.error(f"Error closing client socket: {e}")
+            to_remove_servers = [server_socket for server_socket, timestamp in self.server_heartbeats.items()
+                                 if current_time - timestamp > config.LEADER_HEARTBEAT_TIMEOUT]
+            for server_socket in to_remove_servers:
+                logging.warning(f"Server {server_socket.getpeername()} timed out.")
+                self.remove_server(server_socket)
+
+    def remove_server(self, server_socket):
+        with self.lock:
+            if server_socket in self.server_heartbeats:
+                del self.server_heartbeats[server_socket]
+                logging.info(f"Server {server_socket.getpeername()} removed from heartbeat tracking")
+
+    def is_leader_heartbeat_missing(self):
+        current_time = time.time()
+        with self.lock:
+            for _, timestamp in self.server_heartbeats.items():
+                if current_time - timestamp <= config.LEADER_HEARTBEAT_TIMEOUT:
+                    return False
+            return True
 
     def start_heartbeat_checking(self):
         while True:
             self.check_heartbeats()
-            time.sleep(5)
+            time.sleep(config.HEARTBEAT_INTERVAL)
+
+def send_heartbeat(server_socket):
+    logging.info("Server heartbeat thread started")
+    while True:
+        try:
+            server_socket.sendall(b'HEARTBEAT')
+            time.sleep(config.HEARTBEAT_INTERVAL)
+        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+            logging.error(f"Disconnected from the server. Stopping heartbeat. Error: {e}")
+            break
