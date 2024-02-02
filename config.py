35e4307842aafda89d743ec33d3dd0b16ebe0c7d
Schiruza <150375084+Schiruza@users.noreply.github.com>
2024-02-02 10:14:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
-SERVER_PORT = 8010
-DISCOVERY_PORT = 8011
+base_port = 5000
+max_port = 5005
 
-SERVERS = ['127.0.0.1'] 
+SERVER = "localhost"
 
-HEARTBEAT_INTERVAL = 3  
-LEADER_HEARTBEAT_TIMEOUT = 5 
 
-LCR_TIMEOUT = 5  
-ELECTION_INTERVAL = 5  
-
-LOG_LEVEL = 'INFO'
-LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
