cfc34c9824e28185b38a7770f6f8caa2023c3049
LaurenzBo2000 <152478995+LaurenzBo2000@users.noreply.github.com>
2024-02-04 18:17:40 +0100

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -1,3 +1,4 @@
+#import necessary libraries
 import threading
 import time
 import logging
@@ -6,17 +7,21 @@ import config
 
 logging.basicConfig(level=logging.getLevelName(config.LOG_LEVEL), format=config.LOG_FORMAT)
 
+#Define the HeartbeatChecker class
 class HeartbeatChecker:
+
+    #Initialize the HeartbeatChecker object
     def __init__(self):
         self.server_heartbeats = {}
         self.lock = threading.Lock()
 
+    #Method to update the heartbeat timestamp for a given server
     def update_server_heartbeat(self, server_socket):
         with self.lock:
             self.server_heartbeats[server_socket] = time.time()
             logging.info(f"Server heartbeat updated for {server_socket.getpeername()}")
 
-
+    #Method to check and remove servers with expired heartbeats
     def check_heartbeats(self):
         current_time = time.time()
         with self.lock:
@@ -26,12 +31,14 @@ class HeartbeatChecker:
                 logging.warning(f"Server {server_socket.getpeername()} timed out.")
                 self.remove_server(server_socket)
 
+    #Method to remove a server from the list of tracked heartbeats
     def remove_server(self, server_socket):
         with self.lock:
             if server_socket in self.server_heartbeats:
                 del self.server_heartbeats[server_socket]
                 logging.info(f"Server {server_socket.getpeername()} removed from heartbeat tracking")
 
+    #Method to check if a leader's heartbeat is missing
     def is_leader_heartbeat_missing(self):
         current_time = time.time()
         with self.lock:
@@ -40,11 +47,13 @@ class HeartbeatChecker:
                     return False
             return True
 
+    #Method to start the heartbeat checking loop
     def start_heartbeat_checking(self):
         while True:
             self.check_heartbeats()
             time.sleep(config.HEARTBEAT_INTERVAL)
 
+#Method to send heartbeat messages to the server
 def send_heartbeat(server_socket):
     logging.info("Server heartbeat thread started")
     while True:
